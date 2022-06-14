b26942e470ff3a9ab0fbdafcb5a76903c07f5794
ElvanIpeker <eipekerdogan@hotmail.com>
2022-06-14 21:33:37 +0200

    still working on

--- src/game.js
+++ src/game.js
@@ -1,7 +1,50 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (let x = 0; x <= WIDTH; x += WIDTH / 10) {
+      for (let y = 0; y < HEIGHT; y += HEIGHT / 10) {
+        stroke(1);
+        strokeWeight(3);
+        line(x, 0, x, HEIGHT);
+        line(0, y, WIDTH, y);
+
+      }
+    }
+
+
+
   }
+
+
 }
+
+class Player {
+  constructor(col, row) {
+    this.col = col
+    this.row = row
+
+  }
+
+  draw(img) {
+    image(img, 0, 0)
+
+  }
+
+
+  moveUp() {
+    this.row = this.row - 1
+  }
+  moveDown() {
+    this.row = this.row + 1
+  }
+  moveLeft() {
+    this.col = this.col - 1
+  }
+  moveRight() {
+    this.col = this.col + 1
+
+  }
+
+
+
+}
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
-const game = new Game();
+let game = new Game()
+let player = new Player(0, 0)
+
 
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
-  canvas.parent("canvas");
+  let canvas = createCanvas(WIDTH, WIDTH);
+  canvas.parent("canvas")
+  loadImage("/assets/character-down.png", img => {
+    player.draw(img)
+  })
+
 }
 
 function draw() {
   game.drawGrid();
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
